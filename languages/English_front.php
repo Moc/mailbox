ecb994b667c638b6b1caf2bd67a86670a1b352dd
Tijn Kuyper <github@tijnkuyper.nl>
2016-03-20 20:59:27 +0100

    Added LAN files

---
+++ languages/English_front.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Wrapper - an e107 plugin by Tijn Kuyper
+ *
+ * Copyright (C) 2015-2016 Tijn Kuyper (http://www.tijnkuyper.nl)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
---
+++ languages/English_global.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Wrapper - an e107 plugin by Tijn Kuyper
+ *
+ * Copyright (C) 2015-2016 Tijn Kuyper (http://www.tijnkuyper.nl)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
+
+define("LAN_MESS_NAME",  "Messaging");
+define("LAN_MESS_DESC",  "An up-to-date messaging platform that uses all the latest web technologies and functionalities offered by e107.");
