fdd580620b347593849958844d84d081f37e6f20
Tijn Kuyper <github@tijnkuyper.nl>
2016-03-21 18:48:22 +0100

    Header clean-up

--- e_header.php
+++ e_header.php
@@ -2,7 +2,7 @@
 /*
  * Mailbox - an e107 plugin by Tijn Kuyper
  *
- * Copyright (C) 2015-2016 Tijn Kuyper (http://www.tijnkuyper.nl)
+ * Copyright (C) 2016-2017 Tijn Kuyper (http://www.tijnkuyper.nl)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
