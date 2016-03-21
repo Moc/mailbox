440692683595808b28a75efabc20121a4e4c2d26
Tijn Kuyper <github@tijnkuyper.nl>
2016-03-21 18:41:22 +0100

    Temporary CSS styling

---
+++ e_header.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Mailbox - an e107 plugin by Tijn Kuyper
+ *
+ * Copyright (C) 2015-2016 Tijn Kuyper (http://www.tijnkuyper.nl)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Mailbox plugin CSS
+ *
+ */
+
+if (!defined('e107_INIT')) { exit; }
+
+
+e107::css('mailbox', 'tempstyle.css');
