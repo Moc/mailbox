f74a1f9378e222b8a87c6f894da3fe9dc2e28452
Tijn Kuyper <github@tijnkuyper.nl>
2016-03-22 16:35:40 +0100

    More database structure updates

--- mailbox_sql.php
+++ mailbox_sql.php
@@ -3,9 +3,9 @@ CREATE TABLE mailbox_messages (
   message_from int(10) unsigned NOT NULL default '0',             /* User ID of sender */
   message_to int(10) NOT NULL default '0',                        /* User ID of receiver */
   message_draft int(10) NOT NULL default '0',                     /* Draft status */
-  message_sent tinyint(1) unsigned NOT NULL default '0',			    /* Date sent */
+  message_sent int(10) unsigned NOT NULL default '0',			        /* Date sent */
   message_read int(10) unsigned NOT NULL default '0',			        /* Date read */
-  message_subject text NOT NULL,
+  message_subject varchar(255) NOT NULL,
   message_text text NOT NULL,
   message_from_saved tinyint(1) unsigned NOT NULL default '0',		/* Set when sender has saved the message */
   message_to_saved tinyint(1) unsigned NOT NULL default '0',		  /* Set when receiver has saved the message */
