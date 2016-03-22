5a6d6bdf334a726a37fbd9d310b9cb0887be032f
Tijn Kuyper <github@tijnkuyper.nl>
2016-03-22 12:52:47 +0100

    Start of Friendly URL implementation

---
+++ e_url.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Mailbox - an e107 plugin by Tijn Kuyper
+ *
+ * Copyright (C) 2016-2017 Tijn Kuyper (http://www.tijnkuyper.nl)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
+
+if (!defined('e107_INIT')) { exit; }
+
+class mailbox_url
+{
+	function config()
+	{
+		$config = array();
+
+		$config['mailbox'] = array(
+			'regex'			=> '^mailbox/?$',
+			'sef'			=> 'mailbox',
+			'redirect'		=> '{e_PLUGIN}mailbox/mailbox.php',
+		);
+
+		$config['box'] = array(
+			'regex'			=> '^mailbox/(.*)$',
+			'sef'			=> 'mailbox/{boxname}',
+			'redirect'		=> '{e_PLUGIN}mailbox/mailbox.php?page=$1',
+		);
+
+		$config['read'] = array(
+			'regex'			=> '^mailbox/read/(.*)$',
+			'sef'			=> 'mailbox/read/{id}',
+			'redirect'		=> '{e_PLUGIN}mailbox/read.php?id=$1',
+		);
+
+		return $config;
+	}
+}
