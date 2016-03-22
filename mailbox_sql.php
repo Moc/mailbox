8897c1ece308327b1088363057ef412f81392707
Tijn Kuyper <github@tijnkuyper.nl>
2016-03-22 17:31:05 +0100

    More database structure updates

--- mailbox_sql.php
+++ mailbox_sql.php
@@ -3,14 +3,13 @@ CREATE TABLE mailbox_messages (
   message_from int(10) unsigned NOT NULL default '0',             /* User ID of sender */
   message_to int(10) NOT NULL default '0',                        /* User ID of receiver */
   message_draft int(10) NOT NULL default '0',                     /* Draft status */
-  message_sent int(10) unsigned NOT NULL default '0',			        /* Date sent */
-  message_read int(10) unsigned NOT NULL default '0',			        /* Date read */
-  message_subject varchar(255) NOT NULL,
+  message_sent int(10) unsigned NOT NULL default '0',			        /* Datestamp sent */
+  message_read int(10) unsigned NOT NULL default '0',			        /* Datestamp read */
+  message_subject varchar(255) NOT NULL,          
   message_text text NOT NULL,
-  message_from_saved tinyint(1) unsigned NOT NULL default '0',		/* Set when sender has saved the message */
-  message_to_saved tinyint(1) unsigned NOT NULL default '0',		  /* Set when receiver has saved the message */
-  message_from_deleted tinyint(1) unsigned NOT NULL default '0',  /* Set when sender has deleted the message */
-  message_to_deleted tinyint(1) unsigned NOT NULL default '0',    /* Set when receiver has deleted the message */
+  message_saved tinyint(1) unsigned NOT NULL default '0',		      /* Set when either sender or receiver has saved the message */
+  message_from_deleted int(10) unsigned NOT NULL default '0',     /* Datestamp when sender has deleted the message */
+  message_to_deleted int(10) unsigned NOT NULL default '0',       /* Datestamp when receiver has deleted the message */
   message_attachments text NOT NULL,
   /*message_options varchar(250) NOT NULL default '', */	        /* Options - '+rr' for read receipt - UNDER REVIEW*/
   PRIMARY KEY (message_id)
