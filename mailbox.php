d1c2cd969a6eebed57d1b6e375e4ef7fcccc8936
Tijn Kuyper <github@tijnkuyper.nl>
2016-03-23 20:34:06 +0100

    Fixes to queries to make sure that deleted messages do not show in any mailbox except for trashbox

--- mailbox.php
+++ mailbox.php
@@ -43,16 +43,16 @@ switch ($_GET['page'])
 		$query_getmessages = $sql->retrieve("mailbox_messages", "*", "message_to=".USERID." AND message_to_deleted=0", true); 
 		break;
 	case 'outbox':
-		$query_getmessages = $sql->retrieve("mailbox_messages", "*", "message_from=".USERID."", true);
+		$query_getmessages = $sql->retrieve("mailbox_messages", "*", "message_from=".USERID." AND message_to_deleted=0", true);
 		break;
 	case 'draftbox':
-		$query_getmessages = $sql->retrieve("mailbox_messages", "*", "message_from=".USERID." AND message_draft=1 AND message_sent=0", true);
+		$query_getmessages = $sql->retrieve("mailbox_messages", "*", "message_from=".USERID." AND message_draft=1 AND message_sent=0 AND message_to_deleted=0", true);
 		break;
 	case 'starbox': // no, not Starbucks ;)
-		$query_getmessages = $sql->retrieve("mailbox_messages", "*", "message_to=".USERID." AND message_starred_to=1", true);
+		$query_getmessages = $sql->retrieve("mailbox_messages", "*", "message_to=".USERID." AND message_starred_to=1 AND message_to_deleted=0", true);
 		break;
 	case 'trashbox':
-		$query_getmessages = $sql->retrieve("mailbox_messages", "*", "message_to=".USERID." AND message_to_deleted!=0", true);
+		$query_getmessages = $sql->retrieve("mailbox_messages", "*", "message_to=".USERID." AND message_to_deleted=1", true);
 		break;
 	case 'compose':
 		$text .= "compose page";
