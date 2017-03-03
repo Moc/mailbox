c90dd2e19a020dbcafe79b4c7ac4850d2f4379cc
Tijn Kuyper <github@tijnkuyper.nl>
2017-03-03 18:31:47 +0100

    Cleanup

--- mailbox_shortcodes.php
+++ mailbox_shortcodes.php
@@ -18,8 +18,8 @@ class mailbox_shortcodes extends e_shortcode
       $mailbox_class = new Mailbox;
       $args  = $mailbox_class->get_database_queryargs($parm);
       $count = e107::getDb()->count('mailbox_messages', '(*)', ''.$args.'');
-      
-      return '<span class="label label-primary pull-right">'.$count.'</span>'; 
+
+      return '<span class="label label-primary pull-right">'.$count.'</span>';
    }
 
    function sc_mailbox_boxglyph($parm='')
@@ -33,7 +33,7 @@ class mailbox_shortcodes extends e_shortcode
             $glyph = e107::getParser()->toGlyph('inbox');
             break;
          case 'outbox':
-            $glyph = e107::getParser()->toGlyph('envelope-o'); 
+            $glyph = e107::getParser()->toGlyph('envelope-o');
             break;
          case 'draftbox':
             $glyph = e107::getParser()->toGlyph("pencil-square-o");
@@ -46,8 +46,8 @@ class mailbox_shortcodes extends e_shortcode
             break;
       }
 
-      return $glyph; 
-   } 
+      return $glyph;
+   }
 
    function sc_mailbox_boxlink($parm='')
    {
@@ -58,7 +58,7 @@ class mailbox_shortcodes extends e_shortcode
       );
 
       $url = e107::url('mailbox', 'box', $urlparms);
- 
+
       return $url;
    }
 
@@ -72,9 +72,9 @@ class mailbox_shortcodes extends e_shortcode
       {
          return 'class="active"';
       }
- 
+
       return;
-   } 
+   }
 
    function sc_mailbox_boxtitle($parm='')
    {
@@ -97,15 +97,15 @@ class mailbox_shortcodes extends e_shortcode
             $title = LAN_MAILBOX_TRASHBOX;
             break;
       }
-      
-      return $title; 
+
+      return $title;
    }
 
    function sc_mailbox_composelink($parm='')
    {
-      $url = e107::url('mailbox', 'compose'); 
+      $url = e107::url('mailbox', 'compose');
       return $url;
-   } 
+   }
 
    function sc_mailbox_message_star($parm='')
    {
@@ -133,9 +133,9 @@ class mailbox_shortcodes extends e_shortcode
          case 'draftbox':
             $userinfo = e107::user($this->var['message_to']);
             break;
-      } 
+      }
 
-      return e107::getParser()->toAvatar($userinfo); 
+      return e107::getParser()->toAvatar($userinfo);
    }
 
    function sc_mailbox_message_fromto($parm='')
@@ -152,24 +152,24 @@ class mailbox_shortcodes extends e_shortcode
          case 'draftbox':
             $userinfo = e107::user($this->var['message_to']);
             break;
-      } 
+      }
 
       $profile_link = e107::getUrl()->create('user/profile/view', array('id' => $userinfo['user_id'], 'name' => $userinfo['user_name']));
-      return "<a href='".$profile_link."'>".$userinfo['user_name']."</a>"; 
+      return "<a href='".$profile_link."'>".$userinfo['user_name']."</a>";
    }
 
    function sc_mailbox_message_subject($parm='')
    {
-      // Check for either mailboxes section or reading an individual message 
+      // Check for either mailboxes section or reading an individual message
       if($_GET['page'])
       {
          $urlparms = array(
-            'id' => $this->var['message_id'], 
+            'id' => $this->var['message_id'],
          );
 
          $url = e107::url('mailbox', 'read', $urlparms);
 
-         return "<a href='".$url."'>".$this->var['message_subject']."</a>";   
+         return "<a href='".$url."'>".$this->var['message_subject']."</a>";
       }
       // Reading individual plugin
       else
@@ -181,7 +181,7 @@ class mailbox_shortcodes extends e_shortcode
    function sc_mailbox_message_text($parm='')
    {
       return e107::getParser()->toHTML($this->var['message_text']);
-   } 
+   }
 
    function sc_mailbox_message_attachment($parm='')
    {
@@ -200,24 +200,24 @@ class mailbox_shortcodes extends e_shortcode
       // No need for a date when message is not send yet or when it is a draft message
       if($_GET['page'] == 'draftbox'){ return; }
       if($this->var['message_draft']) { return; }
-            
+
       $gen = e107::getDateConvert();
       if(!$parm) { $parm = 'short'; }
-      
+
       if($parm == 'relative')
       {
-         return $gen->computeLapse($this->var['message_sent'], time(), false, false, 'short');    
+         return $gen->computeLapse($this->var['message_sent'], time(), false, false, 'short');
       }
-      
+
       return $gen->convert_date($this->var['message_sent'], $parm);
    }
 
    // COMPOSE
    function sc_mailbox_compose_to($parm='')
    {
-      $userpicker_options = 
+      $userpicker_options =
       array(
-          'selectize' => 
+          'selectize' =>
              array(
                  'create'  => false,
                  'maxItems'   => 10,
@@ -230,7 +230,7 @@ class mailbox_shortcodes extends e_shortcode
    }
 
    function sc_mailbox_compose_subject($parm='')
-   { 
+   {
       return e107::getForm()->text('message_subject', $subject, '', array('placeholder' => 'Subject'));
    }
 
